d1032da7353b52420274940837c3b6a50552c5af
Hai An <anplhse173639@fpt.edu.vn>
2025-02-08 02:04:56 +0700

    fix

--- app/src/main/java/com/example/myapplication/lab1/Lab1MainActivity.java
+++ app/src/main/java/com/example/myapplication/lab1/Lab1MainActivity.java
@@ -16,7 +16,7 @@ public class Lab1MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_lab1_1b_main);
+        setContentView(R.layout.activity_lab1_2_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
